7920d95192c4978c164598259a63186f9016e204
小塚捷人 <>
2019-10-14 19:54:04 +0900

    GameDefineにマークと色の定義

---
+++ find_client/Assets/Scripts/GameDefine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDefine : MonoBehaviour
+{
+    public enum MarkDefine{
+        MARU = 0,
+        SANKAKU = 1,
+        SHIKAKU = 2
+    }
+
+    public enum ColorDefine
+    {
+        RED = 0,
+        BLUE = 1,
+        GREEN = 2
+    }
+}
